0fc3b389a13421fc2857806d4d3bbbb96b939bda
Valdemir Pinheiro <pinheiroone@gmail.com>
2021-02-10 14:32:38 -0300

    Entrega de projeto

---
+++ DIO.Bank/DIO.Bank/Classes/Conta.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DIO.Bank
+{
+    class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            TipoConta = tipoConta;
+            Saldo = saldo;
+            Credito = credito;
+            Nome = nome;
+        }
+
+        public bool Sacar(double valorSaque)
+        {
+            if(Saldo - valorSaque < (Credito * -1))
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+
+            Saldo -= valorSaque;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", Nome, Saldo);
+            return true;
+        }
+        public void Depositar(double valorDepostio)
+        {
+            Saldo += valorDepostio;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", Nome, Saldo);
+        }
+
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+        {
+            if (Sacar(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta: " + TipoConta + " | ";
+            retorno += "Nome: " + Nome + " | ";
+            retorno += "Saldo: " + Saldo + " | ";
+            retorno += "Crédito: " + Credito;
+            return retorno;
+        }
+    }
+}
